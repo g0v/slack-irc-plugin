41667b9ca1c5efb2486e29c78f0cbce67a0f3769
caasi Huang <caasi.igd@gmail.com>
2021-03-13 22:26:10 +0800

    Add a dumb test for `textFromSlack`

--- middlewares/utils.test.js
+++ middlewares/utils.test.js
@@ -1,3 +1,14 @@
-test('utils', function () {
-  expect(true).toBe(true);
+var utils = require('./utils');
+
+var channels = {
+  'C2PPMRQGP': 'cofacts'
+};
+var users = {
+  'U039CG5S7': 'alice',
+  'U1HJ8FJJJ': 'bob'
+};
+
+it('parse Slack usernames and channel names', function () {
+  var source = '<@U039CG5S7> <@U1HJ8FJJJ> of <#C2PPMRQGP|cofacts>';
+  expect(utils.textFromSlack(source, channels, users)).toBe('@alice @bob of #cofacts');
 });
